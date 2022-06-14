915e5d511414682f96ec43f5b2c5dfde04fa89d0
Mimo <mimoriam420@gmail.com>
2022-06-14 05:19:28 +0500

    Commit #2 - Connected to MongoDB cloud

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 import configparser
 import re
-
+from pymongo import MongoClient
+from bson import ObjectId
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,6 +17,30 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!a help':
+        await message.channel.send("Help not available!")
+
+    if re.match(r"Wordle [0-9]+ [1-6|X]/6", message.content) is not None:
+        # extract the Wordle number from message
+        wordle = message.content.splitlines()[0].split(" ")[1]
+        # extract the score from message
+        score = message.content.splitlines()[0].split(" ")[2][0]
+
+        await message.channel.send()
+
+
 if __name__ == "__main__":
     config.read(ini_path)
-    client.run(config.get('section', 'token'))
+    cluster = MongoClient(config.get("section", "mongo_url"))
+
+    collection = cluster["wordle"]["data"]
+
+    s = collection.find_one({"_id": ObjectId("62a7d0a9a2559438d2907f0e")})
+    print(s)
+
+    client.run(config.get("section", "token"))
