3310270386bd2d13d8c6206f5d21aca02a8580f3
Mimo <mimoriam420@gmail.com>
2022-06-14 11:11:29 +0500

    Commit #9 - Major rewrite into functions, completed!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 import configparser
 import datetime
@@ -8,48 +9,55 @@ from pymongo import MongoClient
 import pymongo.errors
 from bson import ObjectId
 
+description = '''Wordle Bot'''
+
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
-client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents, help_command=None)
+
 config = configparser.ConfigParser()
-ini_path = 'token.ini'
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
 
-@client.event
-async def on_message(message):
+@bot.command(name='help')
+async def _help(ctx):
+    await ctx.send(f"Help not available!")
+
+
+@bot.command(name='score', pass_context=True)
+async def _score(ctx):
     total = []
     total_score = 0
 
-    if message.author == client.user:
+    cursor = collection.find({
+        "username": ctx.message.author.name
+    })
+    for index, doc in enumerate(cursor):
+        total.append(
+            {
+                "username": doc["username"],
+                "game_score": doc["game_score"]
+            }
+        )
+
+    for dict_item in total:
+        for key in dict_item:
+            if dict_item[key].isdigit():
+                total_score += int(dict_item[key])
+
+    await ctx.message.channel.send(f"Your total Wordle score is ``{total_score}``", reference=ctx.message)
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
         return
-
-    if message.content == '!w help':
-        await message.channel.send("Help not available!")
-
-    if message.content == '!w score':
-        cursor = collection.find({
-            "username": message.author.name
-        })
-        for index, doc in enumerate(cursor):
-            total.append(
-                {
-                    "username": doc["username"],
-                    "game_score": doc["game_score"]
-                }
-            )
-
-        for dict_item in total:
-            for key in dict_item:
-                if dict_item[key].isdigit():
-                    total_score += int(dict_item[key])
-
-        await message.channel.send(f"Your total Wordle score is ``{total_score}``", reference=message)
 
     if re.match(r"Wordle [0-9]+ [1-6|X]/6", message.content) is not None:
         wordle = message.content.splitlines()[0].split(" ")[1]
@@ -61,7 +69,7 @@ async def on_message(message):
         }):
             await message.channel.send(f"Can't add wordle score again for today!",
                                        reference=message)
-            return True
+            return
 
         try:
             collection.insert_one(
@@ -75,15 +83,18 @@ async def on_message(message):
             )
         except pymongo.errors.DuplicateKeyError:
             await message.channel.send("Duplication not allowed!", reference=message)
-            return True
+            return
 
         await message.channel.send("Record submitted!", reference=message)
 
+    await bot.process_commands(message)
+
 
 if __name__ == "__main__":
+    ini_path = 'token.ini'
     config.read(ini_path)
 
     cluster = MongoClient(config.get("section", "mongo_url"))
     collection = cluster["wordle"]["data"]
 
-    client.run(config.get("section", "token"))
+    bot.run(config.get("section", "token"))
